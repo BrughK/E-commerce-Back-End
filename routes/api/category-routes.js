ed1b7cd236a6c53338cd6f0bcf4ebf04bcc899dc
Kasen's Desktop <kasenb1211@gmail.com>
2023-02-07 00:20:52 -0500

    finished routes work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+// copying the syntax/general frame from the activities
 
 router.get('/', async (req, res) => {
   // find all categories
@@ -23,14 +24,44 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.json(200).json(categoryData);
+  } catch (err) {
+    res.json(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const updateCategory = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
